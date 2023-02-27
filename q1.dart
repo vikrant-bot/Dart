5fe6882b70e587d537e4403a74570c83bf9f2217
V <80457895+vikrant-bot@users.noreply.github.com>
2023-02-27 07:02:53 +0530

    Update q1.dart

--- q1.dart
+++ q1.dart
@@ -1,7 +1,10 @@
 void main() {
-  int number = 56789;
-  String numberString = number.toString();
-  for (int i = 0; i < numberString.length; i++) {
-    print(numberString[i]);
+  List<int> numbers = [1, 2, 3, 4, 5, 6];
+  Set<int> uniqueNumbers = Set<int>.from(numbers);
+
+  if (numbers.length == uniqueNumbers.length) {
+    print('girls');
+  } else {
+    print('boys');
   }
 }
